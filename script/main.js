cf06ea628042ab47bf7137fc96210473ecf677db
Helio Gabriel <64484809+gabrielcavdias@users.noreply.github.com>
2020-11-30 21:22:27 -0300

    Commit da entrega?

--- script/main.js
+++ script/main.js
@@ -1,5 +1,12 @@
 document.onload = carregarnoticias()
 //alert("Dicas de pesquisa: Dragon ball, Bleach ou Ninku")
+document.querySelector('.buscar').addEventListener('click', () => {
+    let busca = document.querySelector('#busca-anime')
+    buscar(busca.value)
+})  
+//Variáveis globais
+const output = document.querySelector('.output')
+//Funções
 function carregarnoticias(){
     let linkaninws = 'https://enigmatic-sierra-60542.herokuapp.com/https://www.animenewsnetwork.com/'
     fetch(linkaninws).then(resp => resp.text()).then(DOManidb =>{
@@ -10,14 +17,15 @@ function carregarnoticias(){
         for(let i=0;i<5;i++){
             boxNoticias.appendChild(noticias[i])
         }
+        //Arrumando os links
+        document.querySelectorAll('.noticias h3 a').forEach(link => {
+            let anterior = link.attributes[0].value
+            let novo = 'https://www.animenewsnetwork.com'+anterior
+            link.setAttribute('href', novo)
+            link.setAttribute('target', '_blank')
+        })
     })
 }
-
-document.querySelector('.buscar').addEventListener('click', () => {
-    let busca = document.querySelector('#busca-anime')
-    buscar(busca.value)
-})
-const output = document.querySelector('.output')
 function buscar(texto){
     //Limpa o resultado das pesquisas anteriores
     if(document.querySelector('.output ul')!=null){
@@ -61,19 +69,7 @@ function informacoes(caminho){
         }
         console.log(anime)
         montapagina(anime)
-    })
-    
-    /*
-    //Pega a abertura do anime no Youtube
-    let linkytb = `https://enigmatic-sierra-60542.herokuapp.com/https://www.youtube.com/results?search_query=${anime.titulo.replace(' ','+')}+opening`
-    fetch(linkytb).then(resp => resp.text()).then(DOMytb =>{
-        let dom = new DOMParser()
-        let doc = dom.parseFromString(DOMytb,"text/html")
-        //Pega o primeiro link de vídeo, captura o href, o replace é usado para que o vídeo funcione no embed lá na frente na hora de preencher a página
-        let href = doc.querySelector('a#video-title').attributes[3].value.replace('watch?v=', 'embed/')
-        anime.abertura = `https://www.youtube.com${href}`
-    })*/
-
+    })   
 }
 function abrirmodal(){
     let caixamodal = document.createElement('div')
@@ -103,7 +99,14 @@ function montapagina(objanime){
     modal.appendChild(objanime.descricao)
     let personagens = document.createElement('div')
     personagens.classList.add('personagens')
-    //objanime.personagens.forEach(div => personagens.appendChild(div))
-    //modal.appendChild(personagens)
+    objanime.personagens.forEach(div => personagens.appendChild(div))
+    modal.appendChild(personagens)
     modal.appendChild(objanime.equipe)
+    //Arruma os links
+    document.querySelectorAll('.g_bubble a').forEach(link => {
+        let anterior = link.attributes['href'].value
+        let novo = 'https://anidb.net'+anterior
+        link.setAttribute('href', novo)
+        link.setAttribute('target', '_blank')
+    })
 }
